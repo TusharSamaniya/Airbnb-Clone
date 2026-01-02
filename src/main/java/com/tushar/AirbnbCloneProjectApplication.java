fc25ae33e2c590f9b1163ac4a03b812099ec419b
Tushar Samaniya <tusharsamaniya4956@gmail.com>
2026-01-02 18:08:25 +0530

    This is adding the connection to mysql from application. properties file

---
+++ src/main/java/com/tushar/AirbnbCloneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tushar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbCloneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbCloneProjectApplication.class, args);
+	}
+
+}
